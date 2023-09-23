b932243d8d1d51905bfe43e91fd04b4badae601e
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-09-23 15:09:05 +0900

    Fix:  @SpringBootTest에 properties 옵션 추가
    #6

--- src/test/java/jpa/repository/ProductDetailRepositoryTest.java
+++ src/test/java/jpa/repository/ProductDetailRepositoryTest.java
@@ -8,10 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("dev")
+@SpringBootTest(properties = "spring.profiles.active:dev")
 @Slf4j
 public class ProductDetailRepositoryTest {
     @Autowired
