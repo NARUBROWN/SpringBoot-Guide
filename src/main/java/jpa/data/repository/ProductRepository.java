a4b796cc3aa748bec07e8be1f6671af92a980bcb
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-10-10 11:38:57 +0900

    Repo 초기화

--- src/main/java/jpa/data/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package jpa.data.repository;
-
-import jpa.data.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
