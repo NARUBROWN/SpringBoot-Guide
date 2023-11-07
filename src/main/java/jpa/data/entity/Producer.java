e5e585a8ddb802f11e34244a68bd24ba2b97c115
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-11-07 15:02:15 +0900

    [Feat] 다대다 매핑

--- src/main/java/jpa/data/entity/Producer.java
+++ src/main/java/jpa/data/entity/Producer.java
@@ -1,6 +1,9 @@
 package jpa.data.entity;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -10,8 +13,8 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "producer")
-// 다대다 단방향 매핑 연습을 위한 클래스
 public class Producer extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,21 +23,11 @@ public class Producer extends BaseTimeEntity {
 
     private String name;
 
-    // 대다대는 사용하지 않는 방식이다.
     @ManyToMany
     @ToString.Exclude
-    private List<Product> products = new ArrayList<>(); // 리스트를 필드로 가지는 객체에서는 외래키를 가지지 않기 때문에 @JoinColumn을 설정하지 않아도 됨
+    private List<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
-        this.products.add(product);
+        products.add(product);
     }
-
-    @Builder
-    public Producer (Long id, String code, String name, Product product) {
-        this.id = id;
-        this.code = code;
-        this.name = name;
-        addProduct(product);
-    }
-
 }
